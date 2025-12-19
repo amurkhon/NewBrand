d184ac7185214f20e320bdf1a4e4b42f57d06572
amurkhon <akramjonovamurxon02@gmail.com>
2025-12-19 18:14:37 +0900

    fix: modify the system of saving accessToken in cookies

--- src/controllers/member.Controller.ts
+++ src/controllers/member.Controller.ts
@@ -38,6 +38,7 @@ memberController.signup = async (req: Request, res: Response) => {
         res.cookie('accessToken', token, {
             maxAge: AUTH_TIME * 3600 * 1000,
             httpOnly: false,
+            secure: false,
         });
         res.status(HttpCode.OK).json({member: result, accesToken: token});
     } catch (err) {
@@ -57,6 +58,7 @@ memberController.login = async (req: Request, res: Response) => {
         res.cookie('accessToken', token, {
             maxAge: AUTH_TIME * 3600 * 1000,
             httpOnly: false,
+            secure: false,
         });
         res.status(HttpCode.OK).json({member: result, accessToken: token});
     } catch (err) {
